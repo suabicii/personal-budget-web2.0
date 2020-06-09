4f24caaeac0aa2bb3325243f9bc814985ab93246
Suabicii <michal.slabik3@wp.pl>
2020-06-09 17:34:23 +0200

    Fixed login and register functions

---
+++ user-menu.js
@@ -0,0 +1,9 @@
+$("#register").submit(function () {
+  $("#registerModal").modal("hide");
+  $("#registerConfirmation").modal("show");
+});
+
+$("#login").submit(function () {
+  $("#loginModal").modal("hide");
+  $("#loginConfirmation").modal("show");
+});
